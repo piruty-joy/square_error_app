df6bdfc93fb37afa979a0ab9332360ee5f8f0376
piruty-joy <tafuku.5119@gmail.com>
2016-02-02 15:07:47 +0900

    不要なコメントを削除

--- app.py
+++ app.py
@@ -1,39 +1,15 @@
 # coding: utf-8
 
-from flask import Flask, send_file, render_template, request, jsonify
-import numpy as np
-from matplotlib import pyplot
-import string
 import random
+import string
+
+import numpy as np
+from flask import Flask, send_file, render_template, request
+
 import TempImage
 import square_error
-import json
-
 
 app = Flask(__name__)
-
-
-def plot(image):
-	x = np.linspace(0, 10)
-	y = np.sin(x)
-	pyplot.plot(x, y)
-	pyplot.savefig(image, format('png'))
-
-
-@app.route('/image')
-def error_graph():
-	image = square_error.make_picture()
-	chars = string.digits + string.ascii_letters
-	img_name = ''.join(random.choice(chars) for i in range(64)) + '.png'
-
-	with TempImage.TempImage(img_name, image) as img:
-		img.create_png()
-		return send_file(img_name, mimetype='image/png')
-
-
-@app.route('/start')
-def show_start_page():
-	return render_template('index.html')
 
 
 @app.route('/show', methods=['POST'])
@@ -46,27 +22,12 @@ def show():
 
 	with TempImage.TempImage(img_name, image) as img:
 		img.create_png()
-		# return render_template('index.html', img_path=img_name)
 		return send_file(img_name, mimetype='image/png')
-	# image.savefig('static/image/' + img_name)
-	# return render_template('index.html', img_path=img_name)
-
-
-@app.route('/postText', methods=['POST'])
-def lower_conversion():
-	text = request.json['text']
-	if "ping" in text:
-		return_data = {"result": "pong"}
-		return jsonify(ResultSet=json.dumps(return_data))
-	lower_text = text.lower()
-	print(lower_text)
-	return_data = {"result": lower_text}
-	return jsonify(ResultSet=json.dumps(return_data))
 
 
 @app.route('/')
 def index():
-	return render_template('index.html', img_path='bag.jpg')
+	return render_template('index.html')
 
 
 if __name__ == '__main__':
--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,9 @@
 $(window).load(init());
 
 function init() {
-    $("#button").click(function() {
-        var textData = JSON.stringify({"text":$("#input-text").val()});
-        $.ajax({
-            type:'POST',
-            url:'/postText',
-            data:textData,
-            contentType:'application/json',
-            success:function(data) {
-                var result =JSON.parse(data.ResultSet).result;
-                $("#hello").text(result);
-            }
-        });
-        return false;
-    });
-
     $('#num').slider({
         formatter: function(value) {
             return 'Current value: ' + value;
         }
     });
 }
-
-//function changeValue(value) {
-//    document.getElementById("val").innerHTML = value;
-//}
